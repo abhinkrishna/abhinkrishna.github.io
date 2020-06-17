665feb1540628f64a9145e3f1e723d4bdee4986f
Abhin Krishna <abhinkrishna.ka@gmail.com>
2020-06-18 03:11:32 +0530

    UnderConstruction

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Under Construction</h3>
+        <p>Portfolio Website of Abhin Krishna</p>
+        <a
+          className="App-link"
+          href="https://harkdevelopers.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hark Developers
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
