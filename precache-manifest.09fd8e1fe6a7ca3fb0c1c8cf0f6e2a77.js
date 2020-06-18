6d321338999b1ca11a71069023f9e02b603f189a
Abhin Krishna <abhinkrishna.ka@gmail.com>
2020-06-18 11:19:41 +0530

    Updates

---
+++ precache-manifest.09fd8e1fe6a7ca3fb0c1c8cf0f6e2a77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71ab87cd9ea3442a859495a432537b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "308fe313a27a476c65fd",
+    "url": "/static/css/main.86c235aa.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "308fe313a27a476c65fd",
+    "url": "/static/js/main.090ffc83.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "c3579ab0b7e6a2dcd66f41ee44c76c8e",
+    "url": "/static/media/logo.c3579ab0.svg"
+  }
+]);
